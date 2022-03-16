fc0f342754c49fbac778ac7e06e32e0445b4f69c
YeongdoJung <dudeh980@gmail.com>
2022-03-16 09:52:23 +0000

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,5 @@ python main.py --name oisst_trf_rfb4_rmse --data 1 --dataset oisst3 --model oiss
 python main.py --name oisst_trf_rfb4_weightedrmse --data 1 --dataset oisst3 --model oisst_encoder --batch_size 100 --gpu 5 --crit weightedMSE
 python main.py --name oisst_trf_rfb4_gumbel --data 1 --dataset oisst3 --model oisst_encoder --batch_size 100 --gpu 6 --crit GGELV
 python main.py --name oisst_trf_rfb4_frechet --data 1 --dataset oisst3 --model oisst_encoder --batch_size 100 --gpu 7 --crit FGELV
+
+python main.py --name oisst_trf_edit --data 1 --dataset oisst3 --model oisst_encoder_edit --batch_size 10 --gpu 3 --crit FGELV
